153bc6564a36002132edaf6d9b5a9826cfe758a4
sabith <sabith.athavanad@gmail.com>
2022-06-22 09:54:34 +0530

    routers added

---
+++ src/routes/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const AuthService = require("../contoller/auth");
+const TaskService = require("../contoller/task");
+const Validations = require("../middleware/requestValidation");
+const TokenService = require("../middleware/tokenServices");
+
+let routes = (app) => {
+  router.post(
+    "/publicApi/auth/login",
+    Validations.validate("userLogin"),
+    Validations.isParametersValid,
+    AuthService.login
+  );
+
+  router.post(
+    "/secureApi/create-task",
+    TokenService.verifyToken,            //middleware for verifyToken
+    Validations.validate("createTask"), //middleware for validation Schema for JSON request Params
+    Validations.isParametersValid,      //validation middleware for requestParams
+    TaskService.createTask              //controller for the API
+  );
+
+  router.post(
+    "/secureApi/delete-multiple-task",
+    TokenService.verifyToken,
+    Validations.validate("deleteMultipleTask"),
+    Validations.isParametersValid,
+    TaskService.deleteMultipleTask
+  );
+
+  router.post(
+    "/secureApi/delete-task",
+    TokenService.verifyToken,
+    Validations.validate("deleteTask"),
+    Validations.isParametersValid,
+    TaskService.deleteTask
+  );
+
+  app.use(router);
+};
+
+module.exports = routes;
