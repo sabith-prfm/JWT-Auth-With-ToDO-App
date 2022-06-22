189cf07dc05e21479207bafa0b8082f410894809
sabith <sabith.athavanad@gmail.com>
2022-06-22 09:54:47 +0530

    task helper added

---
+++ src/utils/helper.js
@@ -0,0 +1,66 @@
+const conn = require("../connection/connection");
+
+exports.findChildIdsToDelete = async (subTasks, subTaskObj, cb) => {
+  let finalIds = [];
+
+  Object.keys(subTasks).forEach((parentTask, parentIndex) => {
+    let flag = true;
+    //function to loop on subIds
+    function recursiveFunction() {
+      let dbCalls;
+
+      console.log(">>>>subTasks", subTasks);
+      subTasks[parentTask]?.forEach((item) => {
+        // let selectQry = `select * from taskRelation WHERE parentId='${item}'`;
+        let selectQry = `select * from taskRelation INNER JOIN tasks ON taskRelation.parentId=tasks.id WHERE taskRelation.parentId='${item}'`;
+
+        //Promise to fullfill all dbCalls in each level looping
+        dbCalls = new Promise((resolve, reject) => {
+          conn.mySql.query(selectQry, (err, selectResp) => {
+            if (err) reject(err);
+
+            //finding child is in_progress or not
+            let isInProgress = selectResp?.find(
+              (item) => item.status === "IN_PROGRESS"
+            );
+
+            if (isInProgress) {
+              //removing duplicate elements and removing the inProgress child's parentElement
+              finalIds = [...new Set(finalIds)];
+              finalIds.splice(parentIndex, 1);
+              resolve({});
+            } else {
+              //adding and removing duplicates the parent elment if it's child is not in inProgress
+              finalIds.push(item);
+              finalIds = [...new Set(finalIds)];
+
+              //creating next reccursiveFunction's subTasks
+              subTaskObj[item] = selectResp?.map((item) => item.taskId);
+              resolve(subTaskObj);
+            }
+          });
+        });
+      });
+
+      dbCalls
+        ?.then((promiseResp) => {
+          //flattenArrayValues is used to when to stop reccursion
+          let flattenArrayValues = Object.values(promiseResp).flat();
+
+          if (flattenArrayValues?.length > 0) {
+            flag = false;
+            subTaskObj = {};
+            subTasks = promiseResp;
+
+            recursiveFunction();
+          } else {
+            //returning the ids to delete as a CB
+            cb(finalIds);
+          }
+        })
+        .catch((err) => console.log(">>>>>>>>>>>", err));
+    }
+
+    flag && recursiveFunction();
+  });
+};
