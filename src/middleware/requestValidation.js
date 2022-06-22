a511b7b178a82a8cefad4e1b1b9b282a1329f85c
sabith <sabith.athavanad@gmail.com>
2022-06-22 09:54:20 +0530

    parameter validation added

---
+++ src/middleware/requestValidation.js
@@ -0,0 +1,41 @@
+const { body, validationResult } = require("express-validator");
+
+//request JSON Params validation schema
+exports.validate = (method) => {
+  switch (method) {
+    case "userLogin": {
+      return [
+        body("userName", "UserName Required").exists(),
+        body("password", "Password Required").exists(),
+      ];
+    }
+
+    case "createTask": {
+      return [
+        body("taskName", "taskName Required").exists(),
+        body("taskStatus", "taskStatus Required")
+          .exists()
+          .isIn(["PENDING", "IN_PROGRESS", "COMPLETED"]),
+        body("completionDate").optional(),
+      ];
+    }
+
+    case "deleteMultipleTask": {
+      return [body("taskIds", "taskIds Required").exists()];
+    }
+
+    case "deleteTask": {
+      return [body("taskIds", "taskIds Required").exists()];
+    }
+  }
+};
+
+//request JSON validation middleware
+exports.isParametersValid = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } else {
+    next();
+  }
+};
