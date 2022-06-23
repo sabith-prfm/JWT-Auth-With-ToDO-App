22a067702bf58e30333ae16f685833e808ffbb56
sabith <sabith.athavanad@gmail.com>
2022-06-23 06:57:41 +0530

    helper changes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,19 +1,24 @@
 const conn = require("../connection/connection");
 
 exports.findChildIdsToDelete = async (subTasks, subTaskObj, cb) => {
-  let finalIds = [];
+  let finalIds = {};
+  let subTaskCount = Object.keys(subTasks).length;
+  let dummy = {};
 
   Object.keys(subTasks).forEach((parentTask, parentIndex) => {
-    let flag = true;
+    let useUpdatedValue = false;
+    finalIds[parentTask] = [];
     //function to loop on subIds
     function recursiveFunction() {
       let dbCalls;
+      dummy = useUpdatedValue
+        ? Object.values(subTasks).flat()
+        : subTasks[parentTask];
 
-      console.log(">>>>subTasks", subTasks);
-      subTasks[parentTask]?.length > 0 &&
-        subTasks[parentTask]?.forEach((item) => {
-          // let selectQry = `select * from taskRelation WHERE parentId='${item}'`;
-          let selectQry = `select * from taskRelation INNER JOIN tasks ON taskRelation.parentId=tasks.id WHERE taskRelation.parentId='${item}'`;
+      // console.log(">>>>subTasks", subTasks,dummy);
+      dummy.length > 0 &&
+        dummy?.forEach((item) => {
+          let selectQry = `select * from childTable where parentId='${item}'`;
           //Promise to fullfill all dbCalls in each level looping
           dbCalls = new Promise((resolve, reject) => {
             conn.mySql.query(selectQry, (err, selectResp) => {
@@ -24,15 +29,16 @@ exports.findChildIdsToDelete = async (subTasks, subTaskObj, cb) => {
                 (item) => item.status === "IN_PROGRESS"
               );
 
-              if (isInProgress) {
+              if (isInProgress || finalIds[parentTask] === undefined) {
                 //removing duplicate elements and removing the inProgress child's parentElement
-                finalIds = [...new Set(finalIds)];
-                finalIds.splice(parentIndex, 1);
-                resolve({});
+                finalIds[parentTask] = [...new Set(finalIds[parentTask])];
+                let key = parentTask;
+                delete finalIds[key];
+                resolve(finalIds);
               } else {
                 //adding and removing duplicates the parent elment if it's child is not in inProgress
-                finalIds.push(item);
-                finalIds = [...new Set(finalIds)];
+                finalIds[parentTask].push(item);
+                finalIds[parentTask] = [...new Set(finalIds[parentTask])];
 
                 //creating next reccursiveFunction's subTasks
                 subTaskObj[item] = selectResp?.map((item) => item.taskId);
@@ -42,29 +48,31 @@ exports.findChildIdsToDelete = async (subTasks, subTaskObj, cb) => {
           });
         });
 
-      if (dbCalls) {
-        dbCalls
-          .then((promiseResp) => {
-            //flattenArrayValues is used to when to stop reccursion
-            let flattenArrayValues = Object.values(promiseResp).flat();
+      // if (dbCalls) {
+      dbCalls
+        .then((promiseResp) => {
+          // console.log(">>>>>>>>>>>flatten", promiseResp)
+          //flattenArrayValues is used to when to stop reccursion
+          let flattenArrayValues = Object.values(promiseResp).flat();
 
-            if (flattenArrayValues?.length > 0) {
-              flag = false;
-              subTaskObj = {};
-              subTasks = promiseResp;
+          if (flattenArrayValues?.length > 0) {
+            useUpdatedValue = true;
+            subTaskObj = {};
+            subTasks = promiseResp;
 
-              recursiveFunction();
-            } else {
-              //returning the ids to delete as a CB
-              cb(finalIds);
-            }
-          })
-          .catch((err) => console.log(">>>>>>>>>>>", err));
-      } else {
-        cb(finalIds);
-      }
+            recursiveFunction();
+          } else {
+            //returning the ids to delete as a CB
+
+            subTaskCount === parentIndex + 1 && cb(finalIds);
+          }
+        })
+        .catch((err) => console.log(">>>>>>>>>>>", err));
+      // } else {
+      //   subTaskCount === parentIndex + 1 && cb(finalIds);
+      // }
     }
 
-    flag && recursiveFunction();
+    recursiveFunction();
   });
 };
